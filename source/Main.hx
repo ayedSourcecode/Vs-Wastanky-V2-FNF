541bff81e62fcab62f8d458db6d8233b3107384d
ayedFNFmakerSource code <123101780+ayedSourcecode@users.noreply.github.com>
2023-01-29 02:19:41 +0100

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -78,7 +78,7 @@ class Main extends Sprite
 		ClientPrefs.loadDefaultKeys();
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
-		fpsVar = new FPS(10, 3, 0xFFFFFF);
+		fpsVar = new FPS(10, 3, 0xFF00FF);
 		addChild(fpsVar);
 		Lib.current.stage.align = "tl";
 		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
